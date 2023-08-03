fd92c98857004aa3d032344eedb54b994b309088
juner <juner122king@163.com>
2023-08-03 20:10:15 +0800

    使用了JssBridge框架，参考：https://zhuanlan.zhihu.com/p/438763800

--- app/src/main/java/com/lelezu/app/xianzhuan/wxapi/WxLogin.java
+++ app/src/main/java/com/lelezu/app/xianzhuan/wxapi/WxLogin.java
@@ -2,14 +2,24 @@ package com.lelezu.app.xianzhuan.wxapi;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.widget.Toast;
 
+import com.lelezu.app.xianzhuan.R;
 import com.tencent.mm.opensdk.modelmsg.SendAuth;
+import com.tencent.mm.opensdk.modelmsg.SendMessageToWX;
+import com.tencent.mm.opensdk.modelmsg.WXImageObject;
+import com.tencent.mm.opensdk.modelmsg.WXMediaMessage;
+import com.tencent.mm.opensdk.modelmsg.WXWebpageObject;
 import com.tencent.mm.opensdk.openapi.IWXAPI;
 import com.tencent.mm.opensdk.openapi.WXAPIFactory;
 
-public class WxLogin {
 
+public class WxLogin {
+    private static final int THUMB_SIZE = 150;
+
+    private static int mTargetScene = SendMessageToWX.Req.WXSceneSession;
     public static IWXAPI api;
     @SuppressLint("StaticFieldLeak")
     public static Context mContext;
@@ -36,5 +46,68 @@ public class WxLogin {
         api.sendReq(req);
     }
 
+    public static void shareWx(String url) {
+        if (!api.isWXAppInstalled()) {
+            Toast.makeText(mContext, "您还未安装微信客户端", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Bitmap bmp = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.banner1);
+
+        //初始化 WXImageObject 和 WXMediaMessage 对象
+        WXImageObject imgObj = new WXImageObject(bmp);
+        WXMediaMessage msg = new WXMediaMessage();
+        msg.mediaObject = imgObj;
+
+        //设置缩略图
+        Bitmap thumbBmp = Bitmap.createScaledBitmap(bmp, THUMB_SIZE, THUMB_SIZE, true);
+        bmp.recycle();
+        msg.thumbData = Util.bmpToByteArray(thumbBmp, true);
+
+        //构造一个Req
+        SendMessageToWX.Req req = new SendMessageToWX.Req();
+        req.transaction = buildTransaction("img");
+        req.message = msg;
+        req.scene = mTargetScene;
+        //调用api接口，发送数据到微信
+        api.sendReq(req);
+    }
+
+    public static void webWx(String url) {
+        if (!api.isWXAppInstalled()) {
+            Toast.makeText(mContext, "您还未安装微信客户端", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //初始化一个WXWebpageObject，填写url
+        WXWebpageObject webpage = new WXWebpageObject();
+        webpage.webpageUrl = url;
+
+        //用 WXWebpageObject 对象初始化一个 WXMediaMessage 对象
+        WXMediaMessage msg = new WXMediaMessage(webpage);
+        msg.title = mContext.getString(R.string.app_name);
+        msg.description = "点击下载";
+
+//        Bitmap bmp = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.banner1);
+//        Bitmap thumbBmp = Bitmap.createScaledBitmap(bmp, THUMB_SIZE, THUMB_SIZE, true);
+//        bmp.recycle();
+//        msg.thumbData = Util.bmpToByteArray(thumbBmp, true);
+//
+        msg.thumbData = Util.getHtmlByteArray(url);
+
+
+
+        //构造一个Req
+        SendMessageToWX.Req req = new SendMessageToWX.Req();
+        req.transaction = buildTransaction("webpage");
+        req.message = msg;
+        req.scene = mTargetScene;
+
+        //调用api接口，发送数据到微信
+        api.sendReq(req);
+    }
+
+
+    private static String buildTransaction(final String type) {
+        return (type == null) ? String.valueOf(System.currentTimeMillis()) : type + System.currentTimeMillis();
+    }
 
 }
